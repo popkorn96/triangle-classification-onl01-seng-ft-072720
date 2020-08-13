5092d1d5f0c6cfb079bb15e6a2bd723279acf063
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-13 00:28:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c 
+  def initialize(a,b,c)
+    @a = a 
+    @b = b 
+    @c = c
+  end
+
+  def triangle_ex
+    if a.positive? && b.positive? && c.positive?
+      true
+    else
+      false
+    end
+  end
+  
+  def valid?
+    if (a + b > c) && (a + c > b) && (b + c > a)
+      true
+    else 
+      false
+    end
+  end 
+  
+  def equilateral?
+    a == b && a == c && b == c
+  end
+  def scalene? 
+    a != b && a != c && b != c
+  end
+  def isosceles?
+    a == b || a ==c || b == c
+  end
+  def kind 
+    if (triangle_ex == false || valid? == false)
+      raise TriangleError
+    end
+    if equilateral?
+      :equilateral
+    elsif scalene?
+      :scalene
+    elsif isosceles?
+      :isosceles
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
+
